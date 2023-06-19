7791801eb9b6290c32f499809823382a9605e06d
LouisAssaneFaye <louis.faye.18@outlook.it>
2023-06-19 18:16:08 +0200

    main-up terminato

--- src/components/appMain.vue
+++ src/components/appMain.vue
@@ -1,6 +1,17 @@
 <template>
     <main>
-        <cards />
+        
+        <div class="main-up">
+            <select name="languages" id="lang">
+                <option value="Allen">Allen</option>
+                <option value="Other_player">Other_player</option>
+            </select>
+        </div>
+
+        <div class="main-down">
+
+        </div>
+
     </main>
 </template>
 <script>
@@ -19,6 +30,22 @@ export default {
 <style lang="scss" scoped>
 @use '../styles/partials/mixins' as *;
 @use '../styles/partials/variables' as *;
+main{
+    height: calc(100vh - 70px);
+    background-color: $orangeColor;
+    div.main-up{
+        height: 70px;
+        border: 1px solid black;
+        display: flex;
+        align-items: center;
+        select{
+            height: 2rem;
+            width: 9rem;
+            margin-left: 6rem;
+
+        }
+    }
+}
 
     
 </style>
