a753a6616a02286081a78e3619443b204ae85636
LouisAssaneFaye <louis.faye.18@outlook.it>
2023-06-19 19:09:10 +0200

    Struttura base del main terminata

--- src/components/appMain.vue
+++ src/components/appMain.vue
@@ -9,6 +9,20 @@
         </div>
 
         <div class="main-down">
+            <div class="first-container">
+                <div class="second-container">
+                    <div class="second-container-up">
+                        <span>
+                            Found 39 cards
+                        </span>
+                    </div>
+                    <div class="second-container-down">
+                        <cards />
+                    </div>
+
+                </div>
+
+            </div>
 
         </div>
 
@@ -43,6 +57,39 @@ main{
             width: 9rem;
             margin-left: 6rem;
 
+        }
+    }
+
+    div.main-down{
+        border: 1px solid black;
+        height: calc(100% - 70px);
+        div.first-container{
+            height: 100%;
+            width: 86%;
+            border: 1px solid black;
+            margin: 0 auto;
+            background-color: $whiteColor;
+            padding-top: 55px;
+            div.second-container{
+                width: 94%;
+                height: 100%;
+                border: 1px solid black;
+                margin: 0 auto;
+                div.second-container-up{
+                    display: flex;
+                    align-items: center;
+                    height: 55px;
+                    background-color: $blackColor;
+                    color: $whiteColor;
+                    font-size: 1.2rem;
+                    padding-left: 1rem;
+                }
+                div.second-container-down{
+                    height: calc(100% - 50px);
+                    
+                }
+                
+            }
         }
     }
 }
